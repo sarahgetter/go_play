54d60eb5310d82cace2202f84c74e9086722c716
Sarah Therese Getter <sarah.getter@gmail.com>
2023-12-07 21:21:03 -0800

    Delete notebooks/generic_data_loading.py

--- notebooks/generic_data_loading.py
+++
@@ -1,8 +0,0 @@
-# Data Loading
-# Let's load the dataset and take a first look at it.
-#
-# Loading the dataset
-data = pd.read_csv('path/to/your/dataset.csv')
-
-# Displaying the first few rows of the dataset
-data.head()
