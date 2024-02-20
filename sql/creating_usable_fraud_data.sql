af4be7719c4f8cb904eb248416e2b8e0f14b26d9
Sarah Getter <sarah.getter@gmail.com>
2024-02-19 20:50:54 -0800

    update .sql file

--- sql/creating_usable_fraud_data.sql
+++ sql/creating_usable_fraud_data.sql
@@ -1,19 +1,19 @@
 /*Purpose:
 This SQL script is designed to extract relevant data from the fraud_saas_history table in the yourorganization-prod.your_project dataset. It joins this data with information from other tables to provide comprehensive insights into user activity, applied rules, and transaction details related to fraud detection.
 Data Sources:
-* yourorganization-prod.your_dataset.fraud_saas_history: Contains historical data of user activity and applied rules.
-* yourorganization-prod.your_dataset.users: Provides user-related information such as user IDs and emails.
+* yourorganization-prod.your_dataset.fraud_saas_history: Contains historical data of user activity and applied rulef.
+* yourorganization-prod.your_dataset.users: Provides user-related information such as user IDs and emailf.
 * yourorganization-prod.your_dataset.fraud_saas_custom_rules: Stores custom rules added by the fraud department for fraud detection.
 * yourorganization-prod.your_dataset.fraud_saas_transactions: Contains transaction data associated with user activity.
 SQL Queries:
 * 		Data Extraction and Transformation:
     * Extracts relevant fields from the fraud_saas_history table.
-    * Parses JSON arrays (applied_rules) to extract specific rule attributes.
+    * Parses JSON arrays (applied_rules) to extract specific rule attributef.
     * Retrieves additional user information such as user IDs from the users table.
-    * Joins data with custom rules and transaction details.
+    * Joins data with custom rules and transaction detailf.
 * 		Join Conditions:
-    * Joins fraud_saas_history with users based on matching email addresses.
-    * Matches applied rules with custom rules using rule IDs.
+    * Joins fraud_saas_history with users based on matching email addressef.
+    * Matches applied rules with custom rules using rule IDf.
     * Links transaction data based on unique transaction IDs (fraud_saas_id).
 * 		Filtering:
     * Filters out records where user IDs, applied rules, user IDs from the users table, rule IDs, and transaction types are NULL.
@@ -33,13 +33,13 @@ SELECT
   JSON_EXTRACT_SCALAR(applied_rule, '$.id') AS rule_id,
   JSON_EXTRACT_SCALAR(applied_rule, '$.operation') AS rule_operation,
   JSON_EXTRACT_SCALAR(applied_rule, '$.additional_attribute') AS additional_attribute,
-  JSON_EXTRACT_SCALAR(s.device_details, '$.property1') AS device_property1,
-  JSON_EXTRACT_SCALAR(s.device_details, '$.property2') AS device_property2,
-  JSON_EXTRACT_SCALAR(s.email_details, '$.deliverable') AS email_deliverable,
-  JSON_EXTRACT_SCALAR(s.email_details, '$.email') AS email_email,
-  JSON_EXTRACT_SCALAR(s.email_details, '$.account_details.bukalapak.registered') AS email_bukalapak_registered,
-  JSON_EXTRACT_SCALAR(s.ip_details, '$.ip') AS ip_ip,
-  JSON_EXTRACT_SCALAR(s.ip_details, '$.account_details.microsoft.registered') AS ip_microsoft_registered,
+  JSON_EXTRACT_SCALAR(f.device_details, '$.property1') AS device_property1,
+  JSON_EXTRACT_SCALAR(f.device_details, '$.property2') AS device_property2,
+  JSON_EXTRACT_SCALAR(f.email_details, '$.deliverable') AS email_deliverable,
+  JSON_EXTRACT_SCALAR(f.email_details, '$.email') AS email_email,
+  JSON_EXTRACT_SCALAR(f.email_details, '$.account_detailf.bukalapak.registered') AS email_bukalapak_registered,
+  JSON_EXTRACT_SCALAR(f.ip_details, '$.ip') AS ip_ip,
+  JSON_EXTRACT_SCALAR(f.ip_details, '$.account_detailf.microsoft.registered') AS ip_microsoft_registered,
   cr.Flag AS custom_rules_flag,
   cr.ID AS custom_rules_ID,
   cr.Description AS custom_rules_Description,
@@ -49,7 +49,7 @@ SELECT
   t.type AS transaction_type
 FROM 
   `yourorganization-prod.your_dataset.fraud_saas_history` AS f
-LEFT JOIN UNNEST(JSON_EXTRACT_ARRAY(s.applied_rules)) AS applied_rule
+LEFT JOIN UNNEST(JSON_EXTRACT_ARRAY(f.applied_rules)) AS applied_rule
 LEFT JOIN
   `yourorganization-prod.your_dataset.users` AS u ON CONCAT('"', u.email, '"') = JSON_EXTRACT(f.email_details, '$.email')
 LEFT JOIN
